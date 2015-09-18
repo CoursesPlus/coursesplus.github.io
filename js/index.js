2e7ef55352cb990eef47e877b6bfb34a436029d9
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-09-17 21:33:15 -0400

    Add analytics

--- js/index.js
+++ js/index.js
@@ -16,27 +16,33 @@ $(document).ready(function() {
 	$("#scrollStay").scrollToFixed();
 
 	$(".downloadBtn").click(function() {
+		_paq.push(['trackEvent', 'DownloadStart', 'ClickBtn']);
 		if (!isChrome()) {
 			// you're not using Chrome!
+			_paq.push(['trackEvent', 'DownloadFail', 'WrongBrowser', window.navigator.userAgent]);
 			$("#notAvailable").modal();
 			return false;
 		} else if (window.navigator.platform == "MacIntel" && window.screen.width == 1366 && window.screen.height == 768) {
 			// student laptop?
+			_paq.push(['trackEvent', 'DownloadProg', 'SLP']);
 			$("#slpModal").modal();
 			return false;
 		} else {
 			// not student laptop
+			_paq.push(['trackEvent', 'DownloadOK', 'InlineInstall']);
 			chrome.webstore.install();
 			return true;
 		}
 	});
 
 	$(".inlineDl").click(function() {
+		_paq.push(['trackEvent', 'DownloadOK', 'NotSLP']);
 		chrome.webstore.install();
 		window.location.href = "/download";
 	});
 
 	$(".slp").click(function() {
+		_paq.push(['trackEvent', 'DownloadFail', 'SLP']);
 		alert("Thanks for your interest in CoursesPlus! Unfortunately, CoursesPlus is not available on student laptops right now because of a ban on all extensions. We are working with New Lab to get this resolved!");
 		window.location.reload();
 	});
