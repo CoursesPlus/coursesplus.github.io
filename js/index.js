4796889110418479d189e58c510b59d9e29e5d52
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-09-17 21:22:08 -0400

    Check for Google Chrome

--- js/index.js
+++ js/index.js
@@ -1,8 +1,26 @@
+function isChrome() {
+	if (window.navigator.userAgent.match(/Chrome/) != null) {
+		// maybe...
+		if (window.navigator.userAgent.match(/Edge/) != null) {
+			// Microsoft Edge lies and tell websites it's Chrome, Safari, and Edge.
+			// (on mobile, it says it's a Windows Phone and Android phone running Mobile Safari)
+			// Since it can't install Chrome extensions, it's NOT counted as Chrome.
+			return false;
+		}
+		return true;
+	}
+	return false;
+}
+
 $(document).ready(function() {
 	$("#scrollStay").scrollToFixed();
 
 	$(".downloadBtn").click(function() {
-		if (window.navigator.platform == "MacIntel" && window.screen.width == 1366 && window.screen.height == 768) {
+		if (!isChrome()) {
+			// you're not using Chrome!
+			$("#notAvailable").modal();
+			return false;
+		} else if (window.navigator.platform == "MacIntel" && window.screen.width == 1366 && window.screen.height == 768) {
 			// student laptop?
 			$("#slpModal").modal();
 			return false;
